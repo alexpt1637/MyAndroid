63a125d87028f6a061377eeb76746a274c63acc1
alexpt1637 <alexpt1637@yandex.ru>
2017-12-27 21:57:11 +1000

    в файле манифеста для второй активити копируем строки <intent-filter>

--- itProgerLesson5/app/src/main/java/ru/alexpt/android/itprogerlesson5/MainActivity.java
+++ itProgerLesson5/app/src/main/java/ru/alexpt/android/itprogerlesson5/MainActivity.java
@@ -1,17 +1,23 @@
 package ru.alexpt.android.itprogerlesson5;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText pass;
-    private Button btn, btn2;
+    private Button btn, btn2, btn_alert;
+    private RatingBar rating;
+    private TextView text_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
         pass = (EditText)findViewById(R.id.editText);
         btn = (Button)findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
+        btn_alert = (Button) findViewById(R.id.alert);
 
+        // В всплывающей подсказке выводим введенное пользователем значение и меняем цвет фона и текста первой кнопки
         btn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -35,14 +43,15 @@ public class MainActivity extends AppCompatActivity {
                                 MainActivity.this, pass.getText(),
                                 Toast.LENGTH_SHORT
                         ).show();
+                        // выводим вторую всплывающую подсказку
                         Toast.makeText(
                                 MainActivity.this, R.string.end_text,
-                                Toast.LENGTH_LONG
+                                Toast.LENGTH_SHORT
                         ).show();
                     }
                 }
         );
-
+        // Меняем цвет текста и цвет фона первой кнопки при нажатии на вторую, а также выводим другую всплывающую подсказку
         btn2.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -54,6 +63,50 @@ public class MainActivity extends AppCompatActivity {
                                 MainActivity.this, R.string.btn2_text,
                                 Toast.LENGTH_SHORT
                         ).show();
+                    }
+                }
+        );
+
+        btn_alert.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Создаем строитель всплывающих окон методом Builder
+                        AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                        // Создаем всплывающее окно методом setMessage и после сообщения через точку добавляем свойства
+                        a_builder.setMessage("Вы хотите закрыть приложение?")
+                                .setCancelable(false)
+                                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish(); // при нажатии "Да" функция полностью закрывает приложение
+                                    }
+                                })
+                                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel(); // нажатием "Нет" закрываем диалоговое окно
+                                    }
+                                });
+                        // Создаем непосредственно само всплывающее окно
+                        AlertDialog alert = a_builder.create(); // .create() - метод создания всплывающих окон
+                        alert.setTitle("Закрытие приложения");  // сообщение
+                        alert.show(); // функция показа всплывающего окна
+                    }
+                }
+        );
+
+        // Работаем с рейтингом (RatingBar) - добавим его в activity
+        // выше добавляем новую переменную RatingBar и TextView
+        rating = (RatingBar)findViewById(R.id.ratingBar);
+        text_show = (TextView)findViewById(R.id.textView);
+
+        // к rating добавляем функцию, которая будет срабатывать при изменении рейтинга
+        rating.setOnRatingBarChangeListener(
+                new RatingBar.OnRatingBarChangeListener(){
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        text_show.setText("Значение: " + String.valueOf(rating));
                     }
                 }
         );
---
+++ itProgerLesson5/app/src/main/java/ru/alexpt/android/itprogerlesson5/SecondActivity.java
@@ -0,0 +1,31 @@
+package ru.alexpt.android.itprogerlesson5;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+    // Сначала в файле манифеста для второй активити копируем строки <intent-filter>
+    // в них меняем LAUNCHER на DEFAULT
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
