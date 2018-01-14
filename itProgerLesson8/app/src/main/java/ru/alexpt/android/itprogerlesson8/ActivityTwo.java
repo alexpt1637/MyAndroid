89dcc2e64ef9ff43755eaab7861097607958d5ba
alexpt1637 <alexpt1637@yandex.ru>
2018-01-14 22:36:29 +1000

    Создаем контекстное меню

--- itProgerLesson8/app/src/main/java/ru/alexpt/android/itprogerlesson8/ActivityTwo.java
+++ itProgerLesson8/app/src/main/java/ru/alexpt/android/itprogerlesson8/ActivityTwo.java
@@ -2,9 +2,12 @@ package ru.alexpt.android.itprogerlesson8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityTwo extends AppCompatActivity {
 
@@ -24,27 +27,40 @@ public class ActivityTwo extends AppCompatActivity {
         btn2 = (Button) findViewById(R.id.btnTest2);
         btn3 = (Button) findViewById(R.id.btnTest3);
 
+        final Toast toast1 = Toast.makeText(ActivityTwo.this, R.string.textToast1, Toast.LENGTH_SHORT);
+        final Toast toast2 = Toast.makeText(ActivityTwo.this, R.string.textToast2, Toast.LENGTH_SHORT);
+        final Toast toast3 = Toast.makeText(ActivityTwo.this, R.string.textToast3, Toast.LENGTH_SHORT);
+
+        registerForContextMenu(testText);
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 switch (view.getId()) {
                     case R.id.btnTest1:
                         testText.setText(R.string.btnTest1);
+                        toast1.show();
                         break;
                     case R.id.btnTest2:
                         testText.setText(R.string.btnTest2);
+                        toast2.show();
                         break;
                     case R.id.btnTest3:
                         testText.setText(R.string.btnTest3);
+                        toast3.show();
                         break;
                     case R.id.textView1:
                         testText.setText(R.string.btnTest1);
+                        toast1.show();
                         break;
                     case R.id.textView2:
                         testText.setText(R.string.btnTest2);
+                        toast2.show();
                         break;
                     case R.id.textView3:
                         testText.setText(R.string.btnTest3);
+                        toast3.setGravity(Gravity.CENTER, 0, 0);
+                        toast3.show();
                         break;
                 }
             }
@@ -57,5 +73,15 @@ public class ActivityTwo extends AppCompatActivity {
         test2.setOnClickListener(onClickListener);
         test3.setOnClickListener(onClickListener);
 
+    }
+
+    // Создаем контекстное меню
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        switch (v.getId()) {
+            case R.id.textView:
+
+        }
     }
 }
