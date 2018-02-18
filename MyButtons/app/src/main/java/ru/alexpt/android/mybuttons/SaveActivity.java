3fab4d82061d357ccdb2d7e354e0e1d8d881254e
alexpt1637 <alexpt1637@yandex.ru>
2018-02-19 00:31:14 +1000

    button date add

--- MyButtons/app/src/main/java/ru/alexpt/android/mybuttons/SaveActivity.java
+++ MyButtons/app/src/main/java/ru/alexpt/android/mybuttons/SaveActivity.java
@@ -3,6 +3,8 @@ package ru.alexpt.android.mybuttons;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SaveActivity extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class SaveActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save);
 
+        ListView listView = (ListView) findViewById(R.id.listView1);
+
+        // определяем массив типа String
+        final String[] catnames = new String[] { "Рыжик", "Барсик", "Мурзик",
+                "Мурка", "Васька", "Томасина", "Кристина", "Пушок", "Дымка",
+                "Кузя", "Китти", "Масяня", "Симба" };
+
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, catnames);
+
+        listView.setAdapter(adapter);
     }
 }
