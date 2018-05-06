fcd6035d8ac97c1944f307a4a92d9654c1195652
alexpt1637 <alexpt1637@yandex.ru>
2018-05-07 02:09:22 +1000

    Hello Kotlin

---
+++ MyProjects 2018.01-04/HelloKotlin/app/src/main/java/ru/alexpt/android/hellokotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.alexpt.android.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    internal lateinit var mHelloTextView: TextView
+    internal lateinit var mNameEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mHelloTextView = findViewById(R.id.textView)
+        mNameEditText = findViewById(R.id.editText)
+    }
+
+    fun onClick(view: View) {
+        if (mNameEditText.text.length == 0) {
+            mHelloTextView.text = "Hello, Kitty!"
+        } else {
+            mHelloTextView.text = "Привет, " + mNameEditText.text
+        }
+    }
+}
