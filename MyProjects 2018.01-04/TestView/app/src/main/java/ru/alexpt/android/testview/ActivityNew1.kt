7267abb57f5e5fc60f6c6c0afd2217f6fc382ad9
AlexPt <alexpt1637@yandex.ru>
2018-05-24 19:44:25 +1000

    Проверим поле ввода на пустоту

--- MyProjects 2018.01-04/TestView/app/src/main/java/ru/alexpt/android/testview/ActivityNew1.kt
+++ MyProjects 2018.01-04/TestView/app/src/main/java/ru/alexpt/android/testview/ActivityNew1.kt
@@ -53,85 +53,106 @@ class ActivityNew1 : AppCompatActivity() {
 
     // Реализация калькулятора
     @SuppressLint("SetTextI18n")
-    fun onClickSum(view: View){         // Сумма двух чисел
-        blackColor(view)
-        // Текстовое поле преобразовываем в число и помещаем в переменную типа int
-        // значение текстового элемента приводим к строке и далее в число
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
+    fun onClickSum(view: View){                                     // Сумма двух чисел
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)                                         // Если поле ввода пустое
+        } else {                                                    // Если в поле имеется информация
+            blackColor(view)
+            // Текстовое поле преобразовываем в число и помещаем в переменную типа int
+            // значение текстового элемента приводим к строке и далее в число
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
 
-        val resText = num1 + num2
-        mText.text ="Сумма двух чисел: $resText"
-        mIm.setImageResource(R.drawable.smiley01)
-    }
-
-    fun onClickDiff(view: View){        // Разность двух чисел
-        blackColor(view)
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
-
-        val resText = num1 - num2
-        mText.text ="Разность двух чисел: $resText"
-        mIm.setImageResource(R.drawable.smiley02)
-    }
-
-    fun exponentiation(view: View){        // Возведение в степень
-        val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
-        val num2 = (Integer.parseInt(mEl2.text.toString())).toDouble()
-
-        val resText = Math.pow(num1, num2)
-        val str = String.format("%.2f", resText)
-        val num = num2.toInt()
-
-        blackColor(view)
-        when(num2){
-            2.0 -> mText.text = "Квадрат числа: $str"
-            3.0 -> mText.text = "Куб числа: $str"
-            else -> mText.text = "Число $num1 в $num-ой степени: $str"
+            val resText = num1 + num2
+            mText.text ="Сумма двух чисел: $resText"
+            mIm.setImageResource(R.drawable.smiley01)
         }
-        mIm.setImageResource(R.drawable.smiley03)
     }
 
-    fun onMultiply(view: View){         // Функция умножения
-        blackColor(view)
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
+    fun onClickDiff(view: View){                                    // Разность двух чисел
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
 
-        val resText = num1.toLong() * num2
-        mText.text = "Произведение двух чисел: $resText"
-        mIm.setImageResource(R.drawable.smiley04)
+            val resText = num1 - num2
+            mText.text ="Разность двух чисел: $resText"
+            mIm.setImageResource(R.drawable.smiley02)
+        }
     }
 
-    fun noCorrect(view: View){          // Индикация ввода некорректного значения
+    fun exponentiation(view: View){                                 // Возведение в степень
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
+            val num2 = (Integer.parseInt(mEl2.text.toString())).toDouble()
+
+            val resText = Math.pow(num1, num2)
+            val str = String.format("%.2f", resText)
+            val num = num2.toInt()
+
+            blackColor(view)
+            when(num2){
+                2.0 -> mText.text = "Квадрат числа: $str"
+                3.0 -> mText.text = "Куб числа: $str"
+                else -> mText.text = "Число $num1 в $num-ой степени: $str"
+            }
+            mIm.setImageResource(R.drawable.smiley03)
+        }
+    }
+
+    fun onMultiply(view: View){                                     // Функция умножения
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
+
+            val resText = num1.toLong() * num2
+            mText.text = "Произведение двух чисел: $resText"
+            mIm.setImageResource(R.drawable.smiley04)
+        }
+    }
+
+    fun noCorrect(view: View){                      // Индикация ввода некорректного значения
         val mToast = Toast.makeText(this, "Color red!", Toast.LENGTH_SHORT)
         mText.setTextColor(Color.RED)
         mToast.show()
         mText.text = "Incorrect value"
+        imNo(view)
     }
 
-    fun blackColor(view: View){         // Функция начального цвета вывода текста
+    fun blackColor(view: View){                     // Функция начального цвета вывода текста
         val mToast = Toast.makeText(this, "Color black!", Toast.LENGTH_SHORT)
         mText.setTextColor(Color.BLACK)
         mDel.setTextColor(Color.BLACK)
         mToast.show()
     }
 
-    fun onDivide(view: View){           // Функция деления
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
-
-        if(num2 == 0){
+    fun onDivide(view: View){                                       // Функция деления
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
             noCorrect(view)
         } else {
-            blackColor(view)
-            val resText = num1.toDouble() / num2
-            val str = String.format("%.2f", resText)
-            mText.text = "Частное двух чисел: $str"
-            mIm.setImageResource(R.drawable.smiley05)
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
+
+            if(num2 == 0){
+                noCorrect(view)
+            } else {
+                blackColor(view)
+                val resText = num1.toDouble() / num2
+                val str = String.format("%.2f", resText)
+                mText.text = "Частное двух чисел: $str"
+                mIm.setImageResource(R.drawable.smiley05)
+            }
         }
     }
 
-    fun onClickNull(view: View){        // Очистим поле ввода EditText и вывода
+    fun onClickNull(view: View){                    // Очистим поле ввода EditText и вывода
         val mToast = Toast.makeText(this, "Поле очищено!", Toast.LENGTH_SHORT)
         mEl1.text = null
         mEl2.text = null
@@ -142,95 +163,115 @@ class ActivityNew1 : AppCompatActivity() {
         onClickIm(view)
     }
 
-    fun onRoot(view: View){             // Корень квадратный числа
-        blackColor(view)
-        val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
+    fun onRoot(view: View){                                         // Корень квадратный числа
+        if (mEl1.text.length == 0) {                                // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
 
-        val resText = Math.sqrt(num1)
-        val str = String.format("%.3f", resText)
-        mText.text = "Корень квадратный числа: $str"
-        mIm.setImageResource(R.drawable.smiley06)
-    }
-
-    fun onArea(view: View){             // Площадь прямоугольника /квадрата/
-        blackColor(view)
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
-
-        val resText = num1 * num2
-
-        when(num1){
-            num2 -> mText.text = "Площадь квадрата: $resText кв.ед."
-            else -> mText.text = "Площадь прямоугольника: $resText кв.ед."
-        }
-        mIm.setImageResource(R.drawable.smiley07)
-    }
-
-    fun perimeter(view: View){          // Периметр прямоугольника /квадрата/
-        blackColor(view)
-        val num1 = Integer.parseInt(mEl1.text.toString())
-        val num2 = Integer.parseInt(mEl2.text.toString())
-
-        val resText = num1 * 2 + num2 * 2
-
-        when(num1){
-            num2 -> mText.text = "Периметр квадрата: $resText ед."
-            else -> mText.text = "Периметр прямоугольника: $resText ед."
-        }
-        mIm.setImageResource(R.drawable.smiley08)
-    }
-
-    fun hypotenuse(view: View){         // Длина гипотенузы
-        blackColor(view)
-        val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
-        val num2 = (Integer.parseInt(mEl2.text.toString())).toDouble()
-
-        val resText = Math.hypot(num1, num2)
-        val str = String.format("%.2f", resText)
-        mText.text = "Длина гипотенузы: $str ед."
-        mIm.setImageResource(R.drawable.smiley09)
-    }
-
-    fun onFourButtons(view: View){      // Обработка нажатия третьего ряда кнопок
-        val R = (Integer.parseInt(mEl1.text.toString())).toDouble()
-        val resText: Double
-        val str: String
-
-        when(view){
-            mRs -> {
-                blackColor(view)                // Площадь круга
-                mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley00)
-                resText = Math.PI * Math.pow(R, 2.0)
-                str = String.format("%.2f", resText)
-                mText.text = "Площадь круга при R = $R: \n$str кв.ед."}
-            mRl -> {
-                blackColor(view)                // Длина окружности
-                mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley10)
-                resText = 2 * Math.PI * R
-                str = String.format("%.2f", resText)
-                mText.text = "Длина окружности при R = $R: \n$str ед."}
-            mSin -> {if(R > 360){                   // Синус угла
-                noCorrect(view)
-                imNo(view)
-            } else {
-                blackColor(view)
-                mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley11)
-                resText = Math.sin(R)
-                str = String.format("%.3f", resText)
-                mText.text = "sin $R град.: $str" }}
-            mCos -> {if(R > 360){                   // Косинус угла
-                noCorrect(view)
-                imNo(view)
-            } else {
-                blackColor(view)
-                mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley12)
-                resText = Math.cos(R)
-                str = String.format("%.3f", resText)
-                mText.text = "cos $R град.: $str" }}
+            val resText = Math.sqrt(num1)
+            val str = String.format("%.3f", resText)
+            mText.text = "Корень квадратный числа: $str"
+            mIm.setImageResource(R.drawable.smiley06)
         }
     }
 
-    fun onReturn1(view: View){                      // Возврат на главный экран
+    fun onArea(view: View){                                         // Площадь прямоугольника /квадрата/
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
+
+            val resText = num1 * num2
+
+            when(num1){
+                num2 -> mText.text = "Площадь квадрата: $resText кв.ед."
+                else -> mText.text = "Площадь прямоугольника: $resText кв.ед."
+            }
+            mIm.setImageResource(R.drawable.smiley07)
+        }
+    }
+
+    fun perimeter(view: View){                                      // Периметр прямоугольника /квадрата/
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = Integer.parseInt(mEl1.text.toString())
+            val num2 = Integer.parseInt(mEl2.text.toString())
+
+            val resText = num1 * 2 + num2 * 2
+
+            when(num1){
+                num2 -> mText.text = "Периметр квадрата: $resText ед."
+                else -> mText.text = "Периметр прямоугольника: $resText ед."
+            }
+            mIm.setImageResource(R.drawable.smiley08)
+        }
+    }
+
+    fun hypotenuse(view: View){                                     // Длина гипотенузы
+        if (mEl1.text.length == 0 || mEl2.text.length == 0) {       // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            blackColor(view)
+            val num1 = (Integer.parseInt(mEl1.text.toString())).toDouble()
+            val num2 = (Integer.parseInt(mEl2.text.toString())).toDouble()
+
+            val resText = Math.hypot(num1, num2)
+            val str = String.format("%.2f", resText)
+            mText.text = "Длина гипотенузы: $str ед."
+            mIm.setImageResource(R.drawable.smiley09)
+        }
+    }
+
+    fun onFourButtons(view: View){                          // Обработка нажатия третьего ряда кнопок
+        if (mEl1.text.length == 0) {                        // Проверим поле ввода на пустоту
+            noCorrect(view)
+        } else {
+            val R = (Integer.parseInt(mEl1.text.toString())).toDouble()
+            val resText: Double
+            val str: String
+
+            when(view){
+                mRs -> {
+                    blackColor(view)                        // Площадь круга
+                    mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley00)
+                    resText = Math.PI * Math.pow(R, 2.0)
+                    str = String.format("%.2f", resText)
+                    mText.text = "Площадь круга при R = $R: \n$str кв.ед."}
+                mRl -> {
+                    blackColor(view)                        // Длина окружности
+                    mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley10)
+                    resText = 2 * Math.PI * R
+                    str = String.format("%.2f", resText)
+                    mText.text = "Длина окружности при R = $R: \n$str ед."}
+                mSin -> {if(R > 360){                       // Синус угла
+                    noCorrect(view)
+                    imNo(view)
+                } else {
+                    blackColor(view)
+                    mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley11)
+                    resText = Math.sin(R)
+                    str = String.format("%.3f", resText)
+                    mText.text = "sin $R град.: $str" }}
+                mCos -> {if(R > 360){                       // Косинус угла
+                    noCorrect(view)
+                    imNo(view)
+                } else {
+                    blackColor(view)
+                    mIm.setImageResource(ru.alexpt.android.testview.R.drawable.smiley12)
+                    resText = Math.cos(R)
+                    str = String.format("%.3f", resText)
+                    mText.text = "cos $R град.: $str" }}
+            }
+        }
+    }
+
+    fun onReturn1(view: View){                              // Возврат на главный экран
         val mToast = Toast.makeText(this, "Возврат на главный экран выполнен", Toast.LENGTH_SHORT)
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
